7013737f72e0bc42c403b0567a19819427a74287
PulkitAsri <pulkit.asri@gmail.com>
2021-05-14 05:21:14 +0530

    added port and procfile

--- app.js
+++ app.js
@@ -180,6 +180,6 @@ app.get("/about", function (req, res) {
 });
 
 //SERVER 
-app.listen(3000, function () {
+app.listen(process.env.PORT ||3000, function () {
     console.log("Server started on port 3000");
 });
